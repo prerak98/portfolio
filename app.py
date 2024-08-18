f5af2577949870c2efb007bdf89e519dab009b34
prerak98 <prerak.shah3@dcmail.ca>
2024-08-17 22:54:40 -0400

    basic portfolio in flask

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('skills.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
